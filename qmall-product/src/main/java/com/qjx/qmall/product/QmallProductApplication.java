ff211f47bcb2c2eba359b32e4d88a4bb71372f7c
hostgov <zmryanq@gmail.com>
2021-10-07 12:02:42 +1100

    product

---
+++ qmall-product/src/main/java/com/qjx/qmall/product/QmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.qjx.qmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmallProductApplication.class, args);
+	}
+
+}
