1efc8d0a4ac3d54945eb73e96fb2633c2463ce6d
hostgov <zmryanq@gmail.com>
2021-10-11 19:31:49 +1100

    base frame

---
+++ qmall-gateway/src/main/java/com/qjx/qmall/gateway/QmallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.qjx.qmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class QmallGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmallGatewayApplication.class, args);
+	}
+
+}
---
+++ qmall-gateway/src/test/java/com/qjx/qmall/gateway/QmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qjx.qmall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
