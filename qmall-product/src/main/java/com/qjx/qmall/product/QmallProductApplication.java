77c5544a6e816d4e235d2f89403efdc7296c7e22
hostgov <zmryanq@gmail.com>
2021-12-28 15:05:35 +1100

    base frame

--- qmall-product/src/main/java/com/qjx/qmall/product/QmallProductApplication.java
+++ qmall-product/src/main/java/com/qjx/qmall/product/QmallProductApplication.java
@@ -1,16 +1,14 @@
 package com.qjx.qmall.product;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
-@MapperScan("com.qjx.qmall.product.dao")
-@ComponentScan({"com.qjx.qmall"})
+//@MapperScan("com.qjx.qmall.product.dao")
+//@ComponentScan({"com.qjx.qmall"})
 @EnableFeignClients(basePackages = "com.qjx.qmall.product.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
