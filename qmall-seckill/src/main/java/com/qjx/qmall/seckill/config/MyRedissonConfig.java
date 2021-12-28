eeb27ca184928082e2eb6bd5de153533aa985a25
hostgov <zmryanq@gmail.com>
2021-12-28 16:02:25 +1100

    base frame

--- qmall-seckill/src/main/java/com/qjx/qmall/seckill/config/MyRedissonConfig.java
+++ qmall-seckill/src/main/java/com/qjx/qmall/seckill/config/MyRedissonConfig.java
@@ -3,10 +3,9 @@ package com.qjx.qmall.seckill.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
 
 /**
  * Ryan
@@ -15,10 +14,14 @@ import java.io.IOException;
 @Configuration
 public class MyRedissonConfig {
 
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
 	@Bean(destroyMethod = "shutdown")
-	RedissonClient redisson() throws IOException {
+	RedissonClient redisson() {
 		Config config = new Config();
-		config.useSingleServer().setAddress("redis://192.168.56.10:6379");
+//		config.useSingleServer().setAddress("redis://192.168.56.10:6379");
+		config.useSingleServer().setAddress("redis://"+ redisHost + ":6379");
 		return Redisson.create(config);
 	}
 }
